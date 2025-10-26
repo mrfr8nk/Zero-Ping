af813ea1e8253826e954f663c02123aead96ce60
𝐃𝐀𝐑𝐑𝐄𝐋𝐋 𝐌𝐔𝐂𝐇𝐄𝐑𝐈 ⚡ <darrelltakudzwa9@gmail.com>
2025-10-26 11:57:48 +0200

    Change server port from 5000 to 7860

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,7 +15,7 @@ const __dirname = path.dirname(__filename);
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 7860;
 
 // Middleware
 app.use(cors({
